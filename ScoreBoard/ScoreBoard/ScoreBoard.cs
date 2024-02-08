25ee166396671b752ae4c5a996ffc12facb0a383
Krystian <krystian.szadkowski@interia.pl>
2024-02-08 20:34:28 +0100

    Refactor - renamed variables

--- ScoreBoard/ScoreBoard/ScoreBoard.cs
+++ ScoreBoard/ScoreBoard/ScoreBoard.cs
@@ -4,16 +4,16 @@ namespace WorldCupScoreBoard
 {
     public class ScoreBoard
     {
-        private List<Game> _scoreBoardGames = new List<Game>();
+        private List<Game> _gamesInProgress = new List<Game>();
 
         public void StartNewGame(string homeTeamName, string awayTeamName)
         {
-            _scoreBoardGames.Add(new Game(homeTeamName, awayTeamName));
+            _gamesInProgress.Add(new Game(homeTeamName, awayTeamName));
         }
 
         public void UpdateScoreForGame(string homeTeamName, string awayTeamName, int homeTeamGoals, int awayTeamGoals) 
         {
-            var game = _scoreBoardGames.First(g => g.HomeTeamName == homeTeamName && g.AwayTeamName == awayTeamName);
+            var game = _gamesInProgress.First(g => g.HomeTeamName == homeTeamName && g.AwayTeamName == awayTeamName);
 
             game.UpdateScore(homeTeamGoals, awayTeamGoals);
         }
@@ -22,15 +22,15 @@ namespace WorldCupScoreBoard
         {
             var sb = new StringBuilder();
 
-            _scoreBoardGames.Reverse();
+            _gamesInProgress.Reverse();
 
-            var gamesOrderedByTotalScore = _scoreBoardGames.OrderByDescending(g => g.GetTotalScore()).ToList();
+            var gamesOrdered = _gamesInProgress.OrderByDescending(g => g.GetTotalScore()).ToList();
 
-            for (int i = 0; i < gamesOrderedByTotalScore.Count; i++)
+            for (int i = 0; i < gamesOrdered.Count; i++)
             {
-                var game = gamesOrderedByTotalScore[i];
+                var game = gamesOrdered[i];
 
-                if (i < _scoreBoardGames.Count - 1)
+                if (i < _gamesInProgress.Count - 1)
                 {
                     sb.AppendLine($"{i + 1}. {game.HomeTeamName} {game.HomeTeamGoals} - {game.AwayTeamName} {game.AwayTeamGoals}");
                 }
@@ -45,9 +45,9 @@ namespace WorldCupScoreBoard
 
         public void FinishGame(string homeTeamName, string awayTeamName)
         {
-            var game = _scoreBoardGames.First(g => g.HomeTeamName == homeTeamName && g.AwayTeamName == awayTeamName);
+            var game = _gamesInProgress.First(g => g.HomeTeamName == homeTeamName && g.AwayTeamName == awayTeamName);
 
-            _scoreBoardGames.Remove(game);
+            _gamesInProgress.Remove(game);
         }
     }
 }
