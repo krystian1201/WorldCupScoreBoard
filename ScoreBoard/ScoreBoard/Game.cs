91df75d49c9a91799ce10852fb14803a9d48e6e4
Krystian <krystian.szadkowski@interia.pl>
2024-02-08 16:58:12 +0100

    Implementing minimal funcionality to make unit test regarding initial score pass

---
+++ ScoreBoard/ScoreBoard/Game.cs
@@ -0,0 +1,38 @@
+﻿
+namespace WorldCupScoreBoard
+{
+    internal class Game
+    {
+        private string _homeTeamName;
+        public string HomeTeamName  
+        {
+            get { return _homeTeamName; }    
+        } 
+
+        private string _awayTeamName;
+        public string AwayTeamName
+        {
+            get { return _awayTeamName; }
+        }
+
+        private int _homeTeamGoals;
+        public int HomeTeamGoals
+        {
+            get { return _homeTeamGoals; }
+        }
+        
+        private int _awayTeamGoals;
+        public int AwayTeamGoals
+        {
+            get { return _awayTeamGoals; }
+        }
+
+        public Game(string homeTeam, string awayTeam)
+        {
+            _homeTeamName  = homeTeam;
+            _awayTeamName = awayTeam;
+            _homeTeamGoals = 0;
+            _awayTeamGoals = 0;
+        }
+    }
+}
--- ScoreBoard/ScoreBoard/ScoreBoard.cs
+++ ScoreBoard/ScoreBoard/ScoreBoard.cs
@@ -1,7 +1,36 @@
-﻿namespace WorldCupScoreBoard
+﻿using System.Text;
+
+namespace WorldCupScoreBoard
 {
     public class ScoreBoard
     {
+        private List<Game> _scoreBoardGames = new List<Game>();
 
+        public void StartNewGame(string homeTeamName, string awayTeamName)
+        {
+            _scoreBoardGames.Add(new Game(homeTeamName, awayTeamName));
+        }
+
+        public string GetSummary()
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < _scoreBoardGames.Count; i++)
+            {
+                var game = _scoreBoardGames[i];
+
+                if (i < _scoreBoardGames.Count - 1)
+                {
+                    sb.AppendLine($"{i + 1}. {game.HomeTeamName} {game.HomeTeamGoals} - {game.AwayTeamName} {game.AwayTeamGoals}");
+                }
+                else
+                {
+                    sb.Append($"{i + 1}. {game.HomeTeamName} {game.HomeTeamGoals} - {game.AwayTeamName} {game.AwayTeamGoals}");
+                }
+                
+            }
+
+            return sb.ToString();
+        }
     }
 }
