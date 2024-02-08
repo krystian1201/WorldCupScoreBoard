61809ac19d95baba9ea2b681a749d723fc1ac1a2
Krystian <krystian.szadkowski@interia.pl>
2024-02-08 16:19:27 +0100

    Added unit test for initial score when starting a game

---
+++ ScoreBoard/ScoreBoardTests/ScoreBoardTests.cs
@@ -0,0 +1,28 @@
+﻿
+namespace ScoreBoard.UnitTests
+{
+    [TestFixture]
+    public class ScoreBoardTests
+    {
+        private ScoreBoard _scoreBoard;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _scoreBoard = new ScoreBoard();
+        }
+
+        [Test]
+        public void StartNewGame_ReturnsInitialScore_0_0()
+        {
+            //Arrange
+            _scoreBoard.StartNewGame("Mexico", "Canada");
+
+            //Act
+            string scoreBoardSummary = _scoreBoard.GetSummary();
+
+            //Assert
+            Assert.That(scoreBoardSummary, Is.EqualTo("1. Mexico 0 - Canada 0"));
+        }
+    }
+}
